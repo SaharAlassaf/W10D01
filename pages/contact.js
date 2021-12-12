1b0fd207585a804002eb0be00baf0db7f22e9eb2
SaharAlassaf <Sahar.Hussain.elq.js@tuwaiq.edu.sa>
2021-12-12 16:58:37 +0300

    contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,7 @@
 const Contact = () => {
   return (
     <>
-     <Head>
-     <title> facebook | contact us</title>
-     <link rel="icon" href="Generated"/>
       <h1>Contact us</h1>
-      </Head>
     </>
   );
 };
